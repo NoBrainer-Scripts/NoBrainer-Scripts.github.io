a0bb05a0b46f4430c77897b2742b99547f2bf898
ItsANoBrainer <itsanobrainergg@protonmail.com>
2024-01-12 01:17:43 -0500

    fix: sitemapping

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nobrainer-scripts.github.io/',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: './out',
+};
