292e7515dfafc54ff1b8bd3dbb6c8f9847a8256d
ItsANoBrainer <itsanobrainergg@protonmail.com>
2024-01-12 00:19:27 -0500

    Initial Commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>NoBrainer Scripts</span>,
+  project: {
+    link: 'https://github.com/NoBrainer-Scripts/NoBrainer-Scripts.github.io',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/NoBrainer-Scripts/NoBrainer-Scripts.github.io',
+  footer: {
+    text: 'NoBrainer Scripts',
+  },
+}
+
+export default config
